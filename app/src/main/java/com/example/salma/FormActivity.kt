0665ca65d4779ba1d4e87b0963e310a85d85d0fa
salsaden <abaaysalmo@gmail.com>
2023-09-01 20:12:34 +0300

    initial commit

--- app/src/main/java/com/example/salma/FormActivity.kt
+++ app/src/main/java/com/example/salma/FormActivity.kt
@@ -6,14 +6,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -28,6 +32,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -46,6 +51,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -89,11 +95,25 @@ fun myForm(){
     Column(modifier = Modifier.fillMaxSize()) {
 
         //TopAppBar
-        TopAppBar(title = { Text(text = "Form", color = Color.Black, fontSize = 25.sp, fontWeight = FontWeight.Bold)},
+        TopAppBar(title = {
+            Text(
+                text = "Form",
+                color = Color.Black,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
+            )
+        },
             colors = TopAppBarDefaults.largeTopAppBarColors(Color.Cyan),
             navigationIcon = {
-                IconButton(onClick = {  mContext.startActivity(Intent(mContext,ScrollActivity::class.java))}) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription ="arrowback")
+                IconButton(onClick = {
+                    mContext.startActivity(
+                        Intent(
+                            mContext,
+                            ScrollActivity::class.java
+                        )
+                    )
+                }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "arrowback")
                 }
 
             },
@@ -115,108 +135,124 @@ fun myForm(){
             color = Color.Black,
             modifier = Modifier.padding(30.dp)
         )
-       Spacer(modifier = Modifier.height(10.dp))
-        Box(modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center) {
-            Image(painter = painterResource(id = R.drawable.img_32),
-                contentDescription = "",
-                modifier = Modifier.size(100.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+//        Box(modifier = Modifier.fillMaxWidth(),
+//            contentAlignment = Alignment.Center) {
+//            Image(painter = painterResource(id = R.drawable.img_32),
+//                contentDescription = "",
+//                modifier = Modifier.size(100.dp))
 
-        }
+//        }
         //LottieAnimation
         val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.signup))
         val progress by animateLottieCompositionAsState(composition)
-        Box(modifier = Modifier.fillMaxWidth(),
-           contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
 
-        LottieAnimation(composition,progress, modifier = Modifier.size(100.dp))}
+            LottieAnimation(composition, progress, modifier = Modifier.size(100.dp))
+        }
 
-            //Name
-            TextField(
-                value = name,
-                onValueChange = { name = it },
-                modifier = Modifier
-                    .size(width = 350.dp, height = 50.dp)
-                    .padding(start = 10.dp),
-                label = { Text(text = "Name", fontWeight = FontWeight.Bold) },
-                placeholder = { Text(text = "Enter your name") },
-                leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "") },
-                trailingIcon = { Icon(imageVector = Icons.Default.Check, contentDescription = "") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-            )
-            //Email
-            Spacer(modifier = Modifier.height(10.dp))
-            TextField(
-                value = email,
-                onValueChange = { email = it },
-                modifier = Modifier
-                    .size(width = 350.dp, height = 50.dp)
-                    .padding(start = 10.dp),
-                label = { Text(text = "Email", fontWeight = FontWeight.Bold) },
-                placeholder = { Text(text = "Enter your email") },
-                leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
-            )
-            //Password
-            Spacer(modifier = Modifier.height(10.dp))
-            TextField(
-                value = password,
-                onValueChange = { password = it },
-                modifier = Modifier
-                    .size(width = 350.dp, height = 50.dp)
-                    .padding(start = 10.dp),
-                label = { Text(text = "Password", fontWeight = FontWeight.Bold) },
-                placeholder = { Text(text = "Type your password") },
-                leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                visualTransformation = PasswordVisualTransformation()
-            )
-            //Phonumber
-            Spacer(modifier = Modifier.height(10.dp))
-            TextField(
-                value = phonenumber,
-                onValueChange = { phonenumber = it },
-                modifier = Modifier
-                    .size(width = 350.dp, height = 50.dp)
-                    .padding(start = 10.dp),
-                label = { Text(text = "Contact", fontWeight = FontWeight.Bold) },
-                placeholder = { Text(text = "Enter your phonenumber") },
-                leadingIcon = { Icon(imageVector = Icons.Default.Call, contentDescription = "") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            Button(
-                onClick = {
-                    mContext.startActivity(Intent(mContext,LottieAnimationActivity::class.java))
+        //Name
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            modifier = Modifier
+                .size(width = 350.dp, height = 50.dp)
+                .padding(start = 50.dp),
+            label = { Text(text = "Name", fontWeight = FontWeight.Bold) },
+            placeholder = { Text(text = "Enter your name") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "") },
+            trailingIcon = { Icon(imageVector = Icons.Default.Check, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        //Email
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier
+                .size(width = 350.dp, height = 50.dp)
+                .padding(start = 50.dp),
+            label = { Text(text = "Email", fontWeight = FontWeight.Bold) },
+            placeholder = { Text(text = "Enter your email") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        //Password
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier
+                .size(width = 350.dp, height = 50.dp)
+                .padding(start = 50.dp),
+            label = { Text(text = "Password", fontWeight = FontWeight.Bold) },
+            placeholder = { Text(text = "Type your password") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        //Phonumber
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = phonenumber,
+            onValueChange = { phonenumber = it },
+            modifier = Modifier
+                .size(width = 350.dp, height = 50.dp)
+                .padding(start = 50.dp),
+            label = { Text(text = "Contact", fontWeight = FontWeight.Bold) },
+            placeholder = { Text(text = "Enter your phonenumber") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Call, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = {
+                mContext.startActivity(Intent(mContext, LottieAnimationActivity::class.java))
 
-                },
-                modifier = Modifier
-                    .size(width = 350.dp, height = 50.dp)
-                    .padding(start = 10.dp),
-                shape = CutCornerShape(5.dp),
-                colors = ButtonDefaults.buttonColors(Color.Cyan)
-            ) {
+            },
+            modifier = Modifier
+                .size(width = 350.dp, height = 50.dp)
+                .padding(start = 50.dp),
+            shape = CutCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(Color.Cyan)
+        ) {
 
-                Text(
-                    text = "Register",
-                    color = Color.Black,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Spacer(modifier = Modifier.height(15.dp))
             Text(
-                text = "Already have an account?  Login",
-                fontWeight = FontWeight.Bold,
-                fontSize = 15.sp,
-                color = Color.Blue,
-                modifier = Modifier.padding(50.dp)
+                text = "Register",
+                color = Color.Black,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
             )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Row {
+            Text(
+                text = "Already have an account?",
+                fontSize = 15.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold, modifier = Modifier.padding(start = 50.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = "Login",
+                color = Color.Blue,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .clickable {
+                        mContext.startActivity(Intent(mContext, LoginActivity2::class.java))
+                    })
 
-
+        }
     }
+        }
 
-}
+
+
+
+
 
 
 @Preview(showBackground = true)
